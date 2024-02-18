d4068ec0eebb5979ed6a818dc56ca73cce0a4d09
imriyaya <ramune.remonedo@gmail.com>
2024-02-18 21:28:39 +0900

    Add icon menus

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,28 @@
 <script lang="ts" setup>
+function clock(): string {
+  const date = new Date();
+  const week = date.toLocaleString("default", { weekday: "long" });
+  const month = date.toLocaleString("default", { month: "long" });
+  const day_of_month = date.getDate();
+  const hours = (date.getHours().toString()).length>1? date.getHours() : "0"+date.getHours();
+  const minutes = ((date.getMinutes().toString()).length>1? date.getMinutes() : "0"+date.getMinutes());
+  const seconds = ((date.getSeconds().toString()).length>1? date.getSeconds() : "0"+date.getSeconds());
+  return (week + " " + month + " " + day_of_month + " " + hours + ":" + minutes + ":" + seconds);
+}
 </script>
 
 <template>
   <header class="">
     <ol class="menu-items">
-      <li><img alt="🖤" class="item icon" draggable="false" src="https://twemoji.maxcdn.com/v/latest/72x72/1f5a4.png"/>
-      </li>
+      <li><img alt="🖤" class="item favicon" draggable="false" src="https://twemoji.maxcdn.com/v/latest/72x72/1f5a4.png"/></li>
       <li><h1 class="item title">riya.monster</h1></li>
       <li><p class="item">File</p></li>
       <li><p class="item">Edit</p></li>
       <li><p class="item">Help</p></li>
 
+      <ol class="icon-items">
+        <li><p class="item clock" v-text="clock()"></p></li>
+      </ol>
     </ol>
   </header>
 </template>
@@ -25,7 +37,7 @@
   user-drag: none;
 }
 
-.icon {
+.favicon {
   height: 20px;
   width: 30px;
   padding: 3px 6px 0;
@@ -62,6 +74,21 @@
 .menu-items li:hover {
   background-color: rgba(116, 116, 116, 0.321);
 }
+
+.icon-items {
+  display: flex;
+  list-style: none;
+  bottom: 0;
+  overflow: hidden;
+  padding-inline-start: 0px;
+  padding-inline-end: 0px;
+  padding: 0 0%;
+  margin-bottom: 0;
+  text-align: right;
+  margin-left: auto;
+}
+
+
 
 header {
   position: fixed;
