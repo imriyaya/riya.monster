2fe09f6ff3bd4c45f203f6a1ac0a75872e476e0f
imriyaya <ramune.remonedo@gmail.com>
2024-08-05 19:41:34 +0900

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <NuxtLoadingIndicator/>
+    <CommonHeader/>
+    <UContainer class="">
+      <slot/>
+    </UContainer>
+    <CommonFooter/>
+    <UNotifications/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <main>
+    <UContainer class="h-[600px] flex">
+      <div class="my-auto w-auto lg:w-full">
+        <h1 class="text-6xl font-bold backdrop-blur">
+          What's up!<br/>
+          I'm <span class="text-primary backdrop-blur">riya</span>.
+        </h1>
+        <p>
+          
+        </p>
+      </div>
+      <Diamond
+          class="h-[600px] w-screen absolute top-[49px] left-0 -z-10 lg:w-full lg:top-0 lg:relative lg:block lg:z-0"/>
+    </UContainer>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
